953f92478531fee1adadf0118e3d42852eba0da4
Sanchez <osanchez@PANORAMA.COM.PE>
2026-06-12 16:12:56 -0500

    crear tablas postgres

--- app.py
+++ app.py
@@ -2733,13 +2733,12 @@ def generar_mensual_formato():
         download_name=f"control_asistencia_{mes_nombre}_{año}.xlsx"
     )
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     app.run(
-    host="0.0.0.0",
-    port=5000,
-    debug=True
-)
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
